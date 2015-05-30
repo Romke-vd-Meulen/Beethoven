10a7ab15f0d2432fc89dd540abd4662b8245f023
Romke van der Meulen <redge.online@gmail.com>
2015-05-30 08:21:44 -0400

    Also download docker-compose automatically

--- bin/configure_server.sh
+++ bin/configure_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt-get -y install nginx
+
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 cp lib/docker-gen/docker-gen /etc/nginx/docker-gen
 cp lib/docker-gen/docker-gen-upstream-template /etc/nginx/docker-gen-upstream-template
 cp lib/docker-gen/docker-gen-upstream-service /etc/nginx/docker-gen-upstream-service
